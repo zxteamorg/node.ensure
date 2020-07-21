63bbdd55cad2f597691e02f271ff19fbd94c69c7
Max Anurin <maxim.anurin@zxteam.org>
2020-07-21 13:32:53 +0300

    Update to latest zxbuild to embed .map files into the package.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PACKAGE_GUARD: symbol = Symbol.for(packageName);
 if (PACKAGE_GUARD in G) {
 	const conflictVersion = G[PACKAGE_GUARD];
 	// tslint:disable-next-line: max-line-length
-	const msg = `Conflict module version. Look like two different version of package ${packageName} was loaded inside the process: ${conflictVersion} and ${packageVersion}.`;
+	const msg = `Conflict module version. Looks like two different version of package ${packageName} was loaded inside the process: ${conflictVersion} and ${packageVersion}.`;
 	if (process !== undefined && process.env !== undefined && process.env.NODE_ALLOW_CONFLICT_MODULES === "1") {
 		console.warn(msg + " This treats as warning because NODE_ALLOW_CONFLICT_MODULES is set.");
 	} else {
@@ -16,7 +16,6 @@ if (PACKAGE_GUARD in G) {
 }
 
 import * as _ from "lodash";
-
 
 export class EnsureError extends Error {
 	public readonly data: any;
